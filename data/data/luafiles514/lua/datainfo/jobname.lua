6d411bbbc4c377b5c23cd3d77d3f45d6890c7ec7
ya4ept <ya4ept@ya.ru>
2020-10-14 21:45:35 +0300

    2020-10-14 (euRO) - обновили Ragexe.exe !!! - добавили карту egef_dun00 - обновили описание предметов

--- data/data/luafiles514/lua/datainfo/jobname.lua
+++ data/data/luafiles514/lua/datainfo/jobname.lua
@@ -3498,5 +3498,6 @@ JobNameTable = {
 	[jobtbl.JT_EVT_LUNATIC] = "Lunatic",
 	[jobtbl.JT_EVT_FABRE] = "Fabre",
 	[jobtbl.JT_EVT_WILOW] = "Wilow",
-	[jobtbl.JT_EVT_DUSTINESS] = "DUSTINESS"
+	[jobtbl.JT_EVT_DUSTINESS] = "DUSTINESS",
+	[jobtbl.JT_EVT_POISON_SPORE] = "POISON_SPORE"
 }
