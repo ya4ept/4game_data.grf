d2a1806251bfaec19581a93e36980cf2fce8330b
ya4ept <ya4ept@ya.ru>
2021-06-09 21:27:49 +0300

    2021-06-09 (euRO) - обновили предметы

---
+++ System/tb_layer_priority.lua
@@ -0,0 +1,129 @@
+TB_Layer_Priority = {
+	Default_Mid = 200,
+	Default_Top = 400,
+	Default_Bottom = 300,
+	Items_List = {
+		[ACCESSORY_IDs.ACCESSORY_C_Mini_Glasses] = { Default = 503, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Shaving_Cream] = { Default = 300, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Happy_Balloon_J] = { Default = 300, Direction = { [3] = 480, [4] = 480, [5] = 480 } },
+		[ACCESSORY_IDs.ACCESSORY_Love_Cheek] = { Default = 501, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_FlyingGalapago] = { Default = 480, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Little_Aquarium] = { Default = 502, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Whistle] = { Default = 480, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Believers_Cap] = { Default = 80, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_SurviveOrb] = { Default = 480, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Poring_Soap_Pipe] = { Default = 480, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Flying_Drone] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Black_Cat] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Charcoal_Stove] = { Default = 320, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Straight_Long_YL] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Straight_Long_WH] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_White_Rabbit] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Blinking_Eyes] = { Default = 100, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Shiba_Inu] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Darkness_Veil] = { Default = 320, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Wall] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_HAPPY_PARROT] = { Default = 480, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Volume_Low_Twin] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_GOSSIP_RAVEN] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Blinking_Thin_Eyes] = { Default = 100, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Poring_Traffic_Light] = { Default = 480, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Baby_Penguin] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Lolita_Two_Side_Up] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_White_Cat] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Straight_Long_BL] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Mvp] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Orange_Tabby_Cat] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Volume_Low_Twin_WH] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Cat_Ear_Short] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Cat_Ear_Long] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Panda_Rabbit] = { Default = 304, isIgnoreRiding = true },
+		[ACCESSORY_IDs.ACCESSORY_Floating_Ball] = { Default = 304, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_JP_EV15] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_JACK_CASTLE_BAT] = { Default = 480, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Subject_Aura] = { Default = 801, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Subject_Aura_Red] = { Default = 480, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Blessing_Of_Angels] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Morocc_Kid_Servant] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Tone_Of_Gold] = { Default = 802, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Floral_Waltz] = { Default = 480, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Love_Feeling] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Clay_Poring_Jar] = { Default = 450, Direction = { [3] = 79, [4] = 79, [5] = 79 } },
+		[ACCESSORY_IDs.ACCESSORY_C_Piamette_Roll_Hair] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Mobile_Pursuit_System] = { Default = 480, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Electro_Two_Side_Up] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_BLUE_EYESHADOW] = { Default = 100, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Sheep_Horn] = { Default = 200, Direction = { [0] = 410, [1] = 410, [2] = 410, [6] = 410, [7] = 410 } },
+		[ACCESSORY_IDs.ACCESSORY_C_Medium_Wave] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_BELL_PIGEON] = { Default = 480, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Geisha_Make] = { Default = 100, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Fairy_Long_BD] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Fairy_Long_SV] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Straight_Long_BU] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Straight_Long_RD] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Straight_Long_GL] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Straight_Long_GN] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Straight_Long_JBL] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Straight_Long_LPP] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Straight_Long_OM] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Straight_Long_PP] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Miyabi_Long_Hair] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Kururinpa_Hair] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Fluffy_Semi_Long] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Desert_Wolf_B] = { Default = 450, isIgnoreRiding = true, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Alchemist_Square_Bag] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Poring_On_Shoulder] = { Default = 450, Direction = { [3] = 321, [4] = 321, [5] = 321 } },
+		[ACCESSORY_IDs.ACCESSORY_C_Mermaid_Wig] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Picnic_Basket] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Pigtail_Red_Hood] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Smiling_Eyes] = { Default = 100, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Flower_Blossom] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_White_Clay_Filler] = { Default = 100, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Orange_Rabbit] = { Default = 450, isIgnoreRiding = true, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Blinking_Eyes_RD] = { Default = 100, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Medium_Wave_BD] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_Blessing_Sky_Lantern] = { Default = 450, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Miracle_Plant] = { Default = 480, isIgnoreRiding = true, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Volume_Low_Twin_CB] = { Default = 150, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_American_S_hair] = { Default = 450, isIgnoreRiding = true, Direction = {} },
+		[ACCESSORY_IDs.ACCESSORY_C_Fox] = { Default = 304, isIgnoreRiding = true },
+		[ACCESSORY_IDs.ACCESSORY_C_Kishu_Inu] = { Default = 304, isIgnoreRiding = true }
+	},
+	IsIgnoredRidingState = function(acc_id)
+	local Items_List = TB_Layer_Priority.Items_List
+	if nil ~= Items_List then
+		local Item_Tb = Items_List[acc_id]
+		if nil ~= Item_Tb and nil ~= Item_Tb.isIgnoreRiding then
+			return Item_Tb.isIgnoreRiding
+		end
+	end
+	return false
+end
+}
+
+-- Function #0
+GetLayerPriority = function(direction, layer, acc_id)
+	local Items_List = TB_Layer_Priority.Items_List
+	if nil ~= Items_List then
+		local Item_Tb = Items_List[acc_id]
+		if nil ~= Item_Tb then
+			if nil ~= Item_Tb.Direction and nil ~= Item_Tb.Direction[direction] then
+				return Item_Tb.Direction[direction]
+			end
+			return Item_Tb.Default
+		elseif 2 == layer then
+			return TB_Layer_Priority.Default_Bottom
+		elseif 3 == layer then
+			return TB_Layer_Priority.Default_Top
+		elseif 4 == layer then
+			return TB_Layer_Priority.Default_Mid
+		end
+	elseif 2 == layer then
+		return TB_Layer_Priority.Default_Bottom
+	elseif 3 == layer then
+		return TB_Layer_Priority.Default_Top
+	elseif 4 == layer then
+		return TB_Layer_Priority.Default_Mid
+	end
+	return -1
+end
