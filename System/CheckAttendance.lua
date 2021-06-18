8a27408b0ad13720cb5b637200c1e41da08e8cc1
ya4ept <ya4ept@ya.ru>
2021-06-19 01:24:01 +0300

    2021-06-17 (euRO_Prime) - обновили предметы, достижения - добавили админов: 100549, 100009, 100006, 100008, 100423, 100509, 100548, 100005

--- System/CheckAttendance.lua
+++ System/CheckAttendance.lua
@@ -1,39 +1,29 @@
-Config = { StartDate = 20210220, EndDate = 20210312 }
+Config = { StartDate = 20210611, EndDate = 20210619 }
 Reward = {
-	{ 1, 12211, 2 },
-	{ 2, 14534, 5 },
-	{ 3, 14535, 3 },
-	{ 4, 12578, 2 },
-	{ 5, 12404, 12 },
-	{ 6, 12684, 3 },
-	{ 7, 7621, 2 },
-	{ 8, 14537, 2 },
-	{ 9, 14601, 4 },
-	{ 10, 14600, 2 },
-	{ 11, 12208, 1 },
-	{ 12, 14592, 2 },
-	{ 13, 12210, 2 },
-	{ 14, 12209, 10 },
-	{ 15, 100493, 2 },
-	{ 16, 100492, 2 },
-	{ 17, 22979, 2 },
-	{ 18, 12412, 2 },
-	{ 19, 12411, 2 },
-	{ 20, 19842, 1 }
+	{ 1, 12212, 1 },
+	{ 2, 13870, 1 },
+	{ 3, 14289, 1 },
+	{ 4, 16421, 1 },
+	{ 5, 12213, 1 },
+	{ 6, 23344, 2 },
+	{ 7, 12265, 10 }
 }
 
 -- Function #0
 main = function()
 	result, msg = InsertCheckAttendanceConfig(Config.EvendOnOff, Config.StartDate, Config.EndDate)
-	if not result == true then
+	if  not result == true then
 		return false, msg
-	end
-	for k, rewardtbl in pairs(Reward) do
-		result, msg = InsertCheckAttendanceReward(rewardtbl[1], rewardtbl[2], rewardtbl[3])
-		if not result == true then
-			return false, msg
+	else
+		for k, rewardtbl in pairs(Reward) do
+			result, msg = InsertCheckAttendanceReward(rewardtbl[1], rewardtbl[2], rewardtbl[3])
+			if  not result == true then
+				return false, msg
+			else
+				continue
+			end
 		end
+		return true, "success"
+		k = Config.EndDate
 	end
-	return true, "success"
-	k = Config.EndDate
 end
